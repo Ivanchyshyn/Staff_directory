bd24718e02dd5847af725f0008aa8adc15c52fb1
Sasha <ivanchyshyn14@gmail.com>
2019-04-13 16:40:04 +0300

    Added models for Boss and Employees

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Boss, Employee
 
-# Register your models here.
+admin.site.register(Boss)
+admin.site.register(Employee)
--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Boss(models.Model):
+    full_name = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name_plural = 'Bosses'
+
+    def __str__(self):
+        return self.full_name
+
+
+class Employee(models.Model):
+    boss = models.ForeignKey(Boss, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=128)
+    job = models.CharField(max_length=128)
+    start_working_date = models.DateField()
+    salary = models.DecimalField(max_digits=100, decimal_places=2)
+
+    class Meta:
+        verbose_name_plural = 'Staff'
+
+    def __str__(self):
+        return self.full_name
