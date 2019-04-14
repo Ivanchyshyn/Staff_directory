1b76cd3d2e6bf898c569ca08ab773f14fb03d13d
Sasha <ivanchyshyn14@gmail.com>
2019-04-14 16:07:21 +0300

    Added sorting and searching for fields

--- populate_staff.py
+++ populate_staff.py
@@ -11,11 +11,11 @@ from staff.models import Boss, Employee
 
 def populate():
     seeder = Seed.seeder()
-    seeder.add_entity(Boss, 20000, {
+    seeder.add_entity(Boss, 5000, {
         'full_name': lambda x: seeder.faker.first_name() + ' ' + seeder.faker.last_name(),
     })
     seeder.add_entity(Employee, 50000, {
-        'boss': lambda x: Boss.objects.get(id=random.randint(1, 20000)),
+        'boss': lambda x: Boss.objects.get(id=random.randint(1, 5000)),
         'full_name': lambda x: seeder.faker.first_name() + ' ' + seeder.faker.last_name(),
         'job': lambda x: seeder.faker.sentence(),
         'salary': lambda x: random.randrange(1, 10000),
--- staff/views.py
+++ staff/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.db.models import CharField, DecimalField, Q
 from django.views.generic import ListView
 
-from .models import Employee
+from .models import Boss, Employee
 
 class Home(ListView):
-    model = Employee
+    model = Boss
     template_name = 'staff/index.html'
-    context_object_name = 'staff'
+    context_object_name = 'bosses'
 
 
-class Staff(Home):
+class Staff(ListView):
+    model = Employee
     template_name = 'staff/staff.html'
+    context_object_name = 'staff'
+    fields = [f for f in Employee._meta.fields if (isinstance(f, CharField) or isinstance(f, DecimalField))]
+
+    def get(self, request):
+        search_query = request.GET.get('search_box', None)
+        if search_query is not None:
+            queries = [Q(**{f'{f.name}__icontains': search_query}) for f in self.fields]
+            qs = Q()
+            for query in queries:
+                qs = qs | query
+            result = Employee.objects.filter(qs)
+            self.queryset = result
+        return super().get(request)
